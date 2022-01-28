a9e19e9aa76283b4b11bdb6095d65417fefb98f2
Kateryna Talashova <kateryna.talashova@gmail.com>
2022-01-28 14:12:43 +0000

    NotificationMessage class

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,60 @@
 export default class NotificationMessage {
+  static prevNotification;
 
+  constructor (
+    msg = '',
+    {
+      duration = 2000,
+      type = 'success',
+    } = {}) {
+    this.msg = msg;
+    this.duration = duration;
+    this.type = type;
+
+    this.render();
+  }
+
+  getTemplate () {
+    return `
+      <div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">
+            ${this.msg}
+          </div>
+        </div>
+      </div>
+    `
+  }
+
+  render () {
+    const element = document.createElement('div');
+
+    element.innerHTML = this.getTemplate();
+
+    this.element = element.firstElementChild;
+  }
+
+  show (target = document.body) {
+    if(NotificationMessage.prevNotification) {
+      NotificationMessage.prevNotification.remove();
+    }
+
+    NotificationMessage.prevNotification = this.element;
+
+    target.append(this.element);
+    setTimeout(() => this.remove(), this.duration);
+  }
+
+  remove () {
+    this.element.remove();
+  }
+
+  destroy() {
+      if(this.element) {
+          this.remove();
+          this.element = null;
+      }
+  }
 }
